837c2425f2e304d8768faea72375de27e894eaac
陆唯超 <l@dpjia.com>
2020-03-22 13:37:59 +0800

    style: 修改样式

--- src/components/layout/content.js
+++ src/components/layout/content.js
@@ -1,19 +1,16 @@
 import React from 'react'
 import { Layout } from 'antd'
-import { useLocation, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import NonePage from '../../views/404'
 
-
 export default function Content (props) {
-  let location = useLocation()
-  console.log(location)
   let {router} = props
   return (
-    <Layout.Content style={{padding: '0 50px', minHeight: '900px'}} >
+    <Layout.Content style={{padding: '0 5%', minHeight: '900px'}} >
       <Switch>
         {
           router.map((item, index) => {
-            return <Route exact path={item.path} component={item.component} />
+            return <Route key={index} exact path={item.path} component={item.component} />
           })
         }
         <Route component={NonePage}/>
--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -6,7 +6,7 @@ export default function Footer () {
     <Fragment>
       <Affix style={{position: 'fixed', width:'100%', bottom: '0px'}}>
         <Layout.Footer>
-          Copyright ©2019 Created by flashtd1 备案号：<a href="http://www.beian.miit.gov.cn" target="_blank">京ICP备15035828号</a>
+          Copyright ©2019 Created by flashtd1 备案号：<a href="http://www.beian.miit.gov.cn" target="_blank" rel="noopener noreferrer">京ICP备15035828号</a>
         </Layout.Footer>
       </Affix>
     </Fragment>
