92cdee0cd87499ba46c52f2f3e9e5e51f8b9a5fe
陆唯超 <l@dpjia.com>
2020-03-22 13:36:38 +0800

    feat: 调整乐谱页面，将musicxml改成外部传入，修改osmd组件位置

--- src/views/sheet.js
+++ src/views/sheet.js
@@ -1,21 +1,30 @@
-import React, { Fragment } from 'react'
-import Osmd from '../components/osmd'
+import React, { Fragment, useState } from 'react'
+import Osmd from '../components/osmd/osmd'
 import { Row, Col, Card, Button } from 'antd'
 
 function Sheet() {
+    let [xmlPath, setXmlPath] = useState('')
     return (
         <Fragment>
             <Row>
                 <Col span={24}>
                     <Card>
-                        <Button type="primary">读取musicxml</Button>
+                        <Button type="primary" onClick={() => {
+                            if (xmlPath.search('music.xml') > 0) {
+                                setXmlPath(process.env.PUBLIC_URL + '/HelloWorld.xml')
+                            } else {
+                                setXmlPath(process.env.PUBLIC_URL + '/music.xml')
+                            }
+                        }}>读取musicxml</Button>
                     </Card>
                 </Col>
             </Row>
             <Row>
                 <Col span={24}>
                     <Card>
-                        <Osmd />
+                        <Osmd 
+                            xmlPath={xmlPath}
+                        />
                     </Card>
                 </Col>
             </Row>
