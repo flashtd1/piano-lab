74fb21fa2abc3886c58417065d6d7dc78a2933ea
陆唯超 <l@dpjia.com>
2020-03-30 13:23:00 +0800

    feat: 添加了query的判断，决定使用什么hook

--- src/components/staveeditor/vexflow.js
+++ src/components/staveeditor/vexflow.js
@@ -1,13 +1,27 @@
 // import Vex from 'vexflow'
 import React, { useState, useRef, Fragment, useEffect } from 'react'
+import {useLocation, useParams} from 'react-router-dom'
 import { useVexflowInit } from './useNormalVex'
 import { useSimpleInput } from './useSimpleInput'
 import { useSample } from './useSample'
+import querystring from 'querystring'
 
 export default function VexFlow () {
+  let location = useLocation()
   let vexRef = useRef()
+  
+  let qs = querystring.parse(location.search.substring(1))
+  // if (location.search)
+  switch(qs.type) {
+    case 'sample':
+      useSample()
+      break
+    case 'simpleInput':
+      useSimpleInput()
+      break
+  }
   // useSample()
-  useSimpleInput()
+  // useSimpleInput()
 
   return (
     <Fragment>
