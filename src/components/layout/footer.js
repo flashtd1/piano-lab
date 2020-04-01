21f0cae01a295eaa118c8b3b3bb6bc49d1ca2fd3
陆唯超 <l@dpjia.com>
2020-04-01 11:36:13 +0800

    feat: 将底部的ICP备案号改为配置读取,copyright时间改为自动获取

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react'
 import { Layout, Affix } from 'antd'
+import config from '../../config.js'
 
 export default function Footer () {
+  let {ICP} = config
   return (
     <Fragment>
       <Affix style={{position: 'fixed', width:'100%', bottom: '0px'}}>
         <Layout.Footer>
-          Copyright ©2019 Created by flashtd1 备案号：<a href="http://www.beian.miit.gov.cn" target="_blank" rel="noopener noreferrer">京ICP备15035828号</a>
+          Copyright ©{new Date().getFullYear()} Created by flashtd1 备案号：<a href="http://www.beian.miit.gov.cn" target="_blank" rel="noopener noreferrer">{ICP}</a>
         </Layout.Footer>
       </Affix>
     </Fragment>
